4ce53526d14bd90f417480a6d8da90294f418581
Gazuma <deepraut.work@gmail.com>
2025-01-11 01:23:13 +0530

    chore: abstracted database credentials

--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
 const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize('mysql','root','my-secret-pw',{
-    host: 'localhost',
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE,process.env.MYSQL_USER,process.env.MYSQL_PASSWORD,{
+    host: process.env.MYSQL_HOST,
     dialect: 'mysql',
-    port:3306
+    port:process.env.MYSQL_PORT
 })
 
 module.exports = sequelize;
